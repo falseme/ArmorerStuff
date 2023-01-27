35cee346afd2e280a822c43b9f8d395d114a0c8f
Fabricio <fabritomas25@gmail.com>
2023-01-26 21:32:53 -0300

    Torso render & Head position

--- src/com/falseme/render/RenderLoader.java
+++ src/com/falseme/render/RenderLoader.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -25,16 +26,16 @@ public class RenderLoader {
 		ArrayList<Triangle> tris = new ArrayList<>();
 
 		loadHead(tris);
+		loadTorso(tris);
 
 		return tris;
 
 	}
 
-	public static void loadHead(ArrayList<Triangle> tris) {
+	public static void loadHead(List<Triangle> tris) {
 
-		int x = 0;
-		int y = 0;
-		int z = 0;
+		int x = 0, y = 0, z = 0;
+		int[] torsoHeight = { triSize[0] * 10, triSize[1] * 10 }; // no se porque 10 pero bue
 
 		// FROM LEFT TO RIGHT, FROM TOP TO BOTTOM
 		// HEAD (8x8)
@@ -49,7 +50,7 @@ public class RenderLoader {
 				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
 				y = triSize[s] * (i / 8) - (triSize[s] * 4);
 
-				Color color;
+				Color color = Color.WHITE;
 				if (s > 0) {
 					if (skin.getRGB(x / triSize[s] + 44, y / triSize[s] + 12) == 0) // transparent pixel
 						continue;
@@ -58,6 +59,7 @@ public class RenderLoader {
 					color = new Color(skin.getRGB(x / triSize[s] + 12, y / triSize[s] + 12));
 				}
 
+				y -= torsoHeight[s];
 				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
 						new Vertex(x, y + triSize[s], z, 1), color));
 				tris.add(new Triangle(new Vertex(x + triSize[s], y, z, 1),
@@ -77,7 +79,7 @@ public class RenderLoader {
 				y = triSize[s] * (i / 8) - (triSize[s] * 4);
 				z = ((i % 8) * triSize[s]) - (triSize[s] * 4);
 
-				Color color;
+				Color color = Color.WHITE;
 				if (s > 0) { // overlay (x+32)
 					if (skin.getRGB(-z / triSize[s] + 51, y / triSize[s] + 12) == 0)
 						continue;
@@ -86,6 +88,7 @@ public class RenderLoader {
 					color = new Color(skin.getRGB(-z / triSize[s] + 19, y / triSize[s] + 12));
 				}
 
+				y -= torsoHeight[s];
 				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x, y, z + triSize[s], 1),
 						new Vertex(x, y + triSize[s], z, 1), color));
 				tris.add(new Triangle(new Vertex(x, y, z + triSize[s], 1),
@@ -105,7 +108,7 @@ public class RenderLoader {
 				y = triSize[s] * (i / 8) - (triSize[s] * 4);
 				z = ((i % 8) * triSize[s]) - (triSize[s] * 4);
 
-				Color color;
+				Color color = Color.WHITE;
 				if (s > 0) { // overlay
 					if (skin.getRGB(z / triSize[s] + 36, y / triSize[s] + 12) == 0)// transparent pixel
 						continue;
@@ -114,6 +117,7 @@ public class RenderLoader {
 					color = new Color(skin.getRGB(z / triSize[s] + 4, y / triSize[s] + 12));
 				}
 
+				y -= torsoHeight[s];
 				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x, y, z + triSize[s], 1),
 						new Vertex(x, y + triSize[s], z, 1), color));
 				tris.add(new Triangle(new Vertex(x, y, z + triSize[s], 1),
@@ -133,7 +137,7 @@ public class RenderLoader {
 				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
 				y = triSize[s] * (i / 8) - (triSize[s] * 4);
 
-				Color color;
+				Color color = Color.WHITE;
 				if (s > 0) {// overlay
 					if (skin.getRGB(-x / triSize[s] + 59, y / triSize[s] + 12) == 0) // transparent
 						continue;
@@ -142,6 +146,7 @@ public class RenderLoader {
 					color = new Color(skin.getRGB(-x / triSize[s] + 27, y / triSize[s] + 12));
 				}
 
+				y -= torsoHeight[s];
 				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
 						new Vertex(x, y + triSize[s], z, 1), color));
 				tris.add(new Triangle(new Vertex(x + triSize[s], y, z, 1),
@@ -160,7 +165,7 @@ public class RenderLoader {
 				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
 				z = triSize[s] * (i / 8) - (triSize[s] * 4);
 
-				Color color;
+				Color color = Color.WHITE;
 				if (s > 0) { // overlay
 					if (skin.getRGB(x / triSize[s] + 44, z / triSize[s] + 4) == 0) // transparent
 						continue;
@@ -169,6 +174,7 @@ public class RenderLoader {
 					color = new Color(skin.getRGB(x / triSize[s] + 12, z / triSize[s] + 4));
 				}
 
+				y -= torsoHeight[s];
 				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
 						new Vertex(x, y, z + triSize[s], 1), color));
 				tris.add(new Triangle(new Vertex(x + triSize[s], y, z, 1),
@@ -188,13 +194,194 @@ public class RenderLoader {
 				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
 				z = triSize[s] * (i / 8) - (triSize[s] * 4);
 
-				Color color;
+				Color color = Color.WHITE;
 				if (s > 0) { // overlay
 					if (skin.getRGB(x / triSize[s] + 52, z / triSize[s] + 4) == 0)// transparent
 						continue;
 					color = new Color(skin.getRGB(x / triSize[s] + 52, z / triSize[s] + 4));
 				} else { // base
 					color = new Color(skin.getRGB(x / triSize[s] + 20, z / triSize[s] + 4));
+				}
+
+				y -= torsoHeight[s];
+				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x, y, z + triSize[s], 1), color));
+				tris.add(new Triangle(new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x + triSize[s], y, z + triSize[s], 1), new Vertex(x, y, z + triSize[s], 1), color));
+
+			}
+
+		}
+
+	}
+
+	public static void loadTorso(List<Triangle> tris) {
+
+		int x = 0, y = 0, z = 0;
+
+		// FRONT
+		for (int i = 0; i < 96; i++) {
+
+			// base & overlay
+			for (int s = 0; s < 2; s++) {
+
+				z = triSize[s] * 2;
+
+				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
+				y = triSize[s] * (i / 8) - (triSize[s] * 6);
+
+				Color color = Color.WHITE;
+				if (s > 0) { // overlay (y+16)
+					if (skin.getRGB(x / triSize[s] + 24, y / triSize[s] + 42) == 0) // transparent
+						continue;
+					color = new Color(skin.getRGB(x / triSize[s] + 24, y / triSize[s] + 42));
+				} else { // base
+					color = new Color(skin.getRGB(x / triSize[s] + 24, y / triSize[s] + 26));
+				}
+
+				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x, y + triSize[s], z, 1), color));
+				tris.add(new Triangle(new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x + triSize[s], y + triSize[s], z, 1), new Vertex(x, y + triSize[s], z, 1), color));
+
+			}
+
+		}
+
+		// LEFT !![from right to left]!!
+		for (int i = 0; i < 48; i++) {
+
+			// base & overlay
+			for (int s = 0; s < 2; s++) {
+
+				x = triSize[s] * 4;
+
+				y = triSize[s] * (i / 4) - (triSize[s] * 6);
+				z = ((i % 4) * triSize[s]) - (triSize[s] * 2);
+
+				Color color = Color.WHITE;
+				if (s > 0) { // overlay (y+16)
+					if (skin.getRGB(-z / triSize[s] + 29, y / triSize[s] + 42) == 0)
+						continue;
+					color = new Color(skin.getRGB(-z / triSize[s] + 29, y / triSize[s] + 42));
+				} else { // base
+					color = new Color(skin.getRGB(-z / triSize[s] + 29, y / triSize[s] + 26));
+				}
+
+				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x, y, z + triSize[s], 1),
+						new Vertex(x, y + triSize[s], z, 1), color));
+				tris.add(new Triangle(new Vertex(x, y, z + triSize[s], 1),
+						new Vertex(x, y + triSize[s], z + triSize[s], 1), new Vertex(x, y + triSize[s], z, 1), color));
+
+			}
+
+		}
+
+		// RIGHT
+		for (int i = 0; i < 48; i++) {
+
+			// base & overlay
+			for (int s = 0; s < 2; s++) {
+
+				x = -triSize[s] * 4;
+
+				y = triSize[s] * (i / 4) - (triSize[s] * 6);
+				z = ((i % 4) * triSize[s]) - (triSize[s] * 2);
+
+				Color color = Color.WHITE;
+				if (s > 0) { // overlay (y+16)
+					if (skin.getRGB(z / triSize[s] + 18, y / triSize[s] + 42) == 0)
+						continue;
+					color = new Color(skin.getRGB(z / triSize[s] + 18, y / triSize[s] + 42));
+				} else { // base
+					color = new Color(skin.getRGB(z / triSize[s] + 18, y / triSize[s] + 26));
+				}
+
+				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x, y, z + triSize[s], 1),
+						new Vertex(x, y + triSize[s], z, 1), color));
+				tris.add(new Triangle(new Vertex(x, y, z + triSize[s], 1),
+						new Vertex(x, y + triSize[s], z + triSize[s], 1), new Vertex(x, y + triSize[s], z, 1), color));
+
+			}
+
+		}
+
+		// BACK
+		for (int i = 0; i < 96; i++) {
+
+			// base & overlay
+			for (int s = 0; s < 2; s++) {
+
+				z = -triSize[s] * 2;
+
+				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
+				y = triSize[s] * (i / 8) - (triSize[s] * 6);
+
+				Color color = Color.WHITE;
+				if (s > 0) { // overlay (y+16)
+					if (skin.getRGB(x / triSize[s] + 36, y / triSize[s] + 42) == 0) // transparent
+						continue;
+					color = new Color(skin.getRGB(x / triSize[s] + 36, y / triSize[s] + 42));
+				} else { // base
+					color = new Color(skin.getRGB(x / triSize[s] + 36, y / triSize[s] + 26));
+				}
+
+				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x, y + triSize[s], z, 1), color));
+				tris.add(new Triangle(new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x + triSize[s], y + triSize[s], z, 1), new Vertex(x, y + triSize[s], z, 1), color));
+
+			}
+
+		}
+
+		// TOP
+		for (int i = 0; i < 32; i++) {
+
+			// base & overlay
+			for (int s = 0; s < 2; s++) {
+
+				y = -triSize[s] * 6;
+
+				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
+				z = triSize[s] * (i / 8) - (triSize[s] * 2);
+
+				Color color = Color.WHITE;
+				if (s > 0) { // overlay (y+16)
+					if (skin.getRGB(x / triSize[s] + 24, z / triSize[s] + 34) == 0) // transparent
+						continue;
+					color = new Color(skin.getRGB(x / triSize[s] + 24, z / triSize[s] + 34));
+				} else { // base
+					color = new Color(skin.getRGB(x / triSize[s] + 24, z / triSize[s] + 18));
+				}
+
+				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x, y, z + triSize[s], 1), color));
+				tris.add(new Triangle(new Vertex(x + triSize[s], y, z, 1),
+						new Vertex(x + triSize[s], y, z + triSize[s], 1), new Vertex(x, y, z + triSize[s], 1), color));
+
+			}
+
+		}
+
+		// BOTTOM
+		for (int i = 0; i < 32; i++) {
+
+			// base & overlay
+			for (int s = 0; s < 2; s++) {
+
+				y = triSize[s] * 6;
+
+				x = ((i % 8) * triSize[s]) - (triSize[s] * 4);
+				z = triSize[s] * (i / 8) - (triSize[s] * 2);
+
+				Color color = Color.WHITE;
+				if (s > 0) { // overlay (y+16)
+					if (skin.getRGB(x / triSize[s] + 32, z / triSize[s] + 34) == 0) // transparent
+						continue;
+					color = new Color(skin.getRGB(x / triSize[s] + 32, z / triSize[s] + 34));
+				} else { // base
+					color = new Color(skin.getRGB(x / triSize[s] + 32, z / triSize[s] + 18));
 				}
 
 				tris.add(new Triangle(new Vertex(x, y, z, 1), new Vertex(x + triSize[s], y, z, 1),
