7367c6a863a0150e9bf2254595eedadc888c9597
Fabricio <fabritomas25@gmail.com>
2023-08-28 16:30:42 -0300

    Full armor render

--- src/com/falseme/ui/item/Inventory.java
+++ src/com/falseme/ui/item/Inventory.java
@@ -18,46 +18,68 @@ public class Inventory extends JComponent {
 		final int CHESTPLATE_SLOT = 1;
 		final int LEGGINGS_SLOT = 2;
 		final int BOOTS_SLOT = 3;
-		
+
+		// ARMOR
+
 		Item[] helmets = new Item[6];
-		for(int i=0; i<helmets.length; i++) {
+		for (int i = 0; i < helmets.length; i++) {
 			helmets[i] = new Item(ItemType.armor, Assets.ITEM_ARMOR[i][0], i);
 		}
-		
+
 		Item[] chestplates = new Item[6];
-		for(int i=0; i<chestplates.length; i++) {
+		for (int i = 0; i < chestplates.length; i++) {
 			chestplates[i] = new Item(ItemType.armor, Assets.ITEM_ARMOR[i][1], i);
 		}
-		
+
 		Item[] leggings = new Item[6];
-		for(int i=0; i<leggings.length; i++) {
+		for (int i = 0; i < leggings.length; i++) {
 			leggings[i] = new Item(ItemType.armor, Assets.ITEM_ARMOR[i][2], i);
 		}
-		
+
 		Item[] boots = new Item[6];
-		for(int i=0; i<boots.length; i++) {
+		for (int i = 0; i < boots.length; i++) {
 			boots[i] = new Item(ItemType.armor, Assets.ITEM_ARMOR[i][3], i);
 		}
-		
-//		Item[] chestplates = new Item[] { new Item(ItemType.armor, Assets.ITEM_ARMOR[0][1]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[1][1]), new Item(ItemType.armor, Assets.ITEM_ARMOR[2][1]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[3][1]), new Item(ItemType.armor, Assets.ITEM_ARMOR[4][1]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[5][1]) };
-//		
-//		Item[] leggings = new Item[] { new Item(ItemType.armor, Assets.ITEM_ARMOR[0][2]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[1][2]), new Item(ItemType.armor, Assets.ITEM_ARMOR[2][2]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[3][2]), new Item(ItemType.armor, Assets.ITEM_ARMOR[4][2]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[5][2]) };
-//		
-//		Item[] boots = new Item[] { new Item(ItemType.armor, Assets.ITEM_ARMOR[0][3]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[1][3]), new Item(ItemType.armor, Assets.ITEM_ARMOR[2][3]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[3][3]), new Item(ItemType.armor, Assets.ITEM_ARMOR[4][3]),
-//				new Item(ItemType.armor, Assets.ITEM_ARMOR[5][3]) };
-		
+
+		// TRIMS
+
+		Item[] trims = new Item[Assets.RUNE.length];
+		for (int i = 0; i < trims.length; i++) {
+			trims[i] = new Item(ItemType.rune, Assets.RUNE[i], i);
+		}
+
+		// MINERAL (trim-color)
+
+		Item[] minerals = new Item[Assets.MATERIAL.length];
+		for (int i = 0; i < minerals.length; i++) {
+			minerals[i] = new Item(ItemType.mineral, Assets.MATERIAL[i], i);
+		}
+
+		// --- INVENTORY LAYOUT ---//
+
+		// HELMET
+
 		add(new ItemBox(HELMET_SLOT, Assets.ARMOR_FORM[0], helmets));
+		add(new ItemBox(HELMET_SLOT, Assets.RUNE_FORM, trims));
+		add(new ItemBox(HELMET_SLOT, Assets.MATERIAL_FORM, minerals));
+
+		// CHESTPLATE
+
 		add(new ItemBox(CHESTPLATE_SLOT, Assets.ARMOR_FORM[1], chestplates));
+		add(new ItemBox(CHESTPLATE_SLOT, Assets.RUNE_FORM, trims));
+		add(new ItemBox(CHESTPLATE_SLOT, Assets.MATERIAL_FORM, minerals));
+
+		// LEGGINGS
+
 		add(new ItemBox(LEGGINGS_SLOT, Assets.ARMOR_FORM[2], leggings));
+		add(new ItemBox(LEGGINGS_SLOT, Assets.RUNE_FORM, trims));
+		add(new ItemBox(LEGGINGS_SLOT, Assets.MATERIAL_FORM, minerals));
+
+		// BOOTS
+
 		add(new ItemBox(BOOTS_SLOT, Assets.ARMOR_FORM[3], boots));
+		add(new ItemBox(BOOTS_SLOT, Assets.RUNE_FORM, trims));
+		add(new ItemBox(BOOTS_SLOT, Assets.MATERIAL_FORM, minerals));
 
 	}
 
